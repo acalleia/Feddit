2451888216297944dfc5feceb7616ff9dba80440
Alexander Calleia <alex_calleia@yahoo.com>
2017-09-06 20:51:38 -0400

    add forums controller

---
+++ app/controllers/forums_controller.rb
@@ -0,0 +1,22 @@
+class ForumsController < ApplicationController
+
+  def index
+    @forums = Forum.all
+  end
+
+  def show
+    @posts = Post.where(forum_id: params[:id])
+  end
+
+  def create
+    forum = params['forum']
+    Forum.create(subfeddit: forum['subfeddit'])
+    redirect_to forums_path
+  end
+
+  def destroy
+    Forum.destroy(params['id'])
+    redirect_to forums_path
+  end
+
+end
